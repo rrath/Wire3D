515f4f836ee9348017309ece63a771f7f2252fc1
rrath <antibyte@gmail.com>
2014-01-08 14:27:20 +0100

    updated year to 2014

--- Wire/InputSystem/Wii/WireWiiMoteShake.h
+++ Wire/InputSystem/Wii/WireWiiMoteShake.h
@@ -1,6 +1,6 @@
 // Wire3D by Pedro Boechat (pboechat@gmail.com)
 // http://wire3d.googlecode.com
-// Copyright(c) 2009-2013. All rights reserved.
+// Copyright(c) 2009-2014. All rights reserved.
 // 
 // The Wire3D source code is supplied under the terms of the LGPL and
 // may not be copied or disclosed except in accordance with the terms of
